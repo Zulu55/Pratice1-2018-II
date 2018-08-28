767a7101979733688a54446e4243430f0be4ce3f
Juan Zuluaga <carlos.zuluaga@globant.com>
2018-08-27 22:45:26 -0500

    The middle

---
+++ Pratice1-2018-II/Pratice1-2018-II.Domain/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Pratice1_2018_II.Domain.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+
+        public DbSet<Census> Census { get; set; }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
